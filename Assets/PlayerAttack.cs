bf6fccb21bb6b9636214c11a7b9b8f0cbce1a74c
AndriCaba <owens5289@gmail.com>
2024-03-02 00:43:24 +0800

    Effects fx addedddd

---
+++ Assets/PlayerAttack.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+
+    public float attackRange = 1.5f; // Range of the attack
+    public int attackDamage = 100; // Damage dealt per attack
+    public LayerMask enemyLayer; // Layer containing the enemy objects
+    private Transform player;
+
+
+
+
+    //fire script 
+    public Transform firepoint;
+    public Transform bulletParent;
+    public GameObject bulletPrefab;
+    public SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+     void Update()
+    {
+       
+    }
+
+    public void Attack()
+    {
+
+        
+        Shoot();
+
+    }
+    void Shoot()
+    {
+
+
+
+        Instantiate(bulletPrefab, firepoint.position, Quaternion.identity);
+
+    }
+}
+
