ce4e9aa4adafdcd6b9a595a74d07e7bbfd9f6099
AndriCaba <106049169+AndriCaba@users.noreply.github.com>
2024-03-02 22:18:01 +0800

    Dont touch

---
+++ Assets/AttackStateManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackStateManager : MonoBehaviour
+{
+    public float attackRange = 1.5f; // Range of the attack
+    public int attackDamage = 100; // Damage dealt per attack
+    public LayerMask enemyLayer; // Layer containing the enemy objects
+   /* private Transform player;*/
+
+
+
+
+    //fire script 
+    public Transform firepoint;
+    public Transform bulletParent;
+    public GameObject bulletPrefab;
+    public SpriteRenderer spriteRenderer;
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            Attack();
+        }
+    }
+
+    public void Attack()
+    {
+
+
+        Shoot();
+
+    }
+    void Shoot()
+    {
+
+
+
+        Instantiate(bulletPrefab, firepoint.position, Quaternion.identity);
+      
+
+    }
+
+}
